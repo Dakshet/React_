3ac995446887f1f94f15e263d3edcccbb9633b59
Dakshet <dakshghole@gmail.com>
2023-09-05 15:56:04 +0530

    Add on github

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");  //Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+  }
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#09325a"
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+      // setInterval(() =>{
+      //   document.title = "TextUtils is Amazing Mode"
+      // },2000)
+      // setInterval(() =>{
+      //   document.title = "Install TextUtils Now"
+      // },1500)
+    }
+    else {
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      showAlert("Light mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+  return (
+    <>
+      {/* <Navbar title="Amazon" about="About Us"/>
+            <Navbar/> */}
+      {/* <Router> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
+
+          {/* /users -->  Component1
+              /users/home -->  Component2 */}
+
+          {/* <Routes>
+            <Route path="/"> */}
+              {/* index means go to the same path i.e. =>  / */}
+              {/* <Route exact index element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+              {/* <Route exact index element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+              <Route exact path='about' element={<About />} />   */}
+            {/* </Route>
+          </Routes> */}
+        </div>
+      {/* </Router> */}
+
+      {/* <TextForm heading="King Text" /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercse was cilcked" + text)
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Uppercase!", "success")
+    }
+
+    const handleLowClick = () => {
+        // console.log("Uppercse was cilcked" + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to Lowercase!", "success")
+    }
+
+    const handleClearClick = () => {
+        // console.log("Uppercse was cilcked" + text)
+        let newText = "";
+        setText(newText)
+    }
+
+    const handleCopyTextClick = () => {
+        const text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Copy the text sucessfully!", "success")
+    }
+
+    const handleRemoveExtraSpacesClick = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+        props.showAlert("Removing the all spacess!", "success")
+    }
+
+    const handleExtractEmailClick = () => {
+        const vari = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+        if(vari){
+            setText(vari.join('\n'));
+        }
+        else{
+            const text = document.getElementById("myBox");
+            text.select();
+        }
+
+        
+    }
+
+    const handleOnChange = (evt) => {
+        // console.log("Change")
+        setText(evt.target.value)
+    }
+
+    const [text, setText] = useState("")
+    // text = "new text";          //Wrong way to change the state
+    // setText("new text");        //Correct way to change the state
+    return (
+        <>
+            <div className='container' style={{color: props.mode==="light"?"grey":"white"}}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor: props.mode==="dark"?"grey":"white", color: props.mode==="light"?"grey":"white"}} id="myBox" rows="8"></textarea>
+                </div>
+                <button className='btn btn-primary mx-3' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
+                <button className='btn btn-primary mx-3' onClick={handleClearClick}>Clear</button>
+                <button className='btn btn-primary mx-3' onClick={handleCopyTextClick}>Copy</button>
+                <button className='btn btn-primary mx-3' onClick={handleRemoveExtraSpacesClick}>Removing Space</button>
+                <button className='btn btn-primary mx-3' onClick={handleExtractEmailClick}>Extract EmailID</button>
+            </div>
+            <div className="container my-3" style={{color: props.mode==="light"?"grey":"white"}}>
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length-1} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter something to preview it here"}</p>
+                <h2>Email Id:</h2>
+                <p>{text.length>0?((text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi))  === null ? "In this text no email Id present":(text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi) + "")):"Enter something to preview it here"}</p>
+            </div>
+        </>
+    )
+}
+
