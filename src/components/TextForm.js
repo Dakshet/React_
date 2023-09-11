519cffe0b93ad34c9bfa5f160a53db1485721b07
Dakshet <dakshghole@gmail.com>
2023-09-11 12:42:07 +0530

    Solve the problem of layout shifting and solve the problem of cursor that not showing on textarea

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -61,7 +61,7 @@ export default function TextForm(props) {
             <div className='container' style={{color: props.mode==="light"?"grey":"white"}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor: props.mode==="dark"?"grey":"white", color: props.mode==="light"?"grey":"white"}} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor: props.mode==="dark"?"#13466e":"white", color: props.mode==="light"?"grey":"white"}} id="myBox" rows="8"></textarea>
                 </div>
                 <button className='btn btn-primary mx-3 my-2' onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className='btn btn-primary mx-3 my-2' onClick={handleLowClick}>Convert to Lowercase</button>
