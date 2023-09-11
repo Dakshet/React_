8a0304dedced2db9e9b97f76d34866963e29fee3
Dakshet <dakshghole@gmail.com>
2023-09-11 13:35:06 +0530

    Solve the problem of copy text because when go to the next line that time that word is not measure so here problem solve

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,10 +23,7 @@ export default function TextForm(props) {
     }
 
     const handleCopyTextClick = () => {
-        const text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value)
-        document.getSelection().removeAllRanges()
+        navigator.clipboard.writeText(text)
         props.showAlert("Copy the text sucessfully!", "success")
     }
 
@@ -73,7 +70,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{color: props.mode==="light"?"grey":"white"}}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:"Enter something to preview it here"}</p>
