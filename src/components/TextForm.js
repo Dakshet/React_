6cfd200bb19ce6662dd68895e1d1a8655a7651fd
Dakshet <dakshghole@gmail.com>
2023-09-11 12:13:04 +0530

    Fix the problem of space that is major as word

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -72,7 +72,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{color: props.mode==="light"?"grey":"white"}}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length-1} words and {text.length} characters</p>
+                <p>{text.split(" ").filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:"Enter something to preview it here"}</p>
